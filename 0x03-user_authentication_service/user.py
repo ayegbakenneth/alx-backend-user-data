5633e11d27e5a41b7b07f0f5ae475950db74e8e3
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-14 15:18:00 +0000

    New correction on how to create a model class for a database table

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,11 +6,13 @@ from sqlalchemy.ext.declarative import declarative_base
 """ Module importation path """
 Base = declarative_base()
 
+
 class User(Base):
+    """ Class model of users in db """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
