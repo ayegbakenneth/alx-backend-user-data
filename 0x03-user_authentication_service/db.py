5a9a8f7e86fd0135a4b998d1aa3c4208c6bbbbbd
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-14 16:12:13 +0000

    A database class that adds a new user to the database table user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" File executable path """
+
+import logging
+from typing import Dict
+from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+from user import Base, User
+""" Module importation path """
+
+logging.disable(logging.WARNING)
+
+
+class DB:
+    """ A db class that add a user to the db """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        # Create new user and add it to the db.
+        new_user = User(email=email, hashed_password=hashed_password)
+        try:
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception as e:
+            print(f"Error adding user to database: {e}")
+            self._session.rollback()
+            raise
+        return new_user
