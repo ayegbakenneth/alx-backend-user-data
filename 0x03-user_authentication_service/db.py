fb2fb9084beb244b2b5b91bc0f6e8f8cbdbb6544
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-15 08:04:17 +0000

    Another correction on the db class features

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -61,22 +61,16 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """ This method helps to update a user details """
         try:
-            # Find the user with the given user ID
             user = self.find_user_by(id=user_id)
         except NoResultFound:
             raise ValueError("User with id {} not found".format(user_id))
 
-        # Update user's attributes
         for key, value in kwargs.items():
             if not hasattr(user, key):
-                # Raise error if an argument that does not correspond to a user
-                # attribute is passed
                 raise ValueError("User has no attribute {}".format(key))
             setattr(user, key, value)
 
         try:
-            # Commit changes to the database
             self._session.commit()
         except InvalidRequestError:
-            # Raise error if an invalid request is made
             raise ValueError("Invalid request")
