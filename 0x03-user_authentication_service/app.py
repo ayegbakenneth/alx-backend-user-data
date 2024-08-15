f778ba0d6b4da8e41ea3211489a66c58048f4cb2
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-15 11:39:26 +0000

    The correct implementation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """ File  executable path """
 
-from flask import Flask, jasonify
+from flask import Flask, jsonify
 """ Module importation path """
 
 
 app = Flask(__name__)
 
 
-@app.route("/", method=["GET"])
-def home():
-    return jasonify({"message": "Bienvenue"})
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    return jsonify({"message": "Bienvenue"})
 
 
-if __name__ = ("__main__"):
+if __name__ == ("__main__"):
     app.run(host="0.0.0.0", port="5000")
