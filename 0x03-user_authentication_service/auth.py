8a7b9537863121a41a71b48a4f88a144540039d4
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-15 10:13:14 +0000

    A method to register a new user into the database

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,3 +12,22 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     password_hashed = bcrypt.hashpw(encoded_password, salt)
     return password_hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(email: str, password: str) -> User:
+        """ A method that returns a user object
+        after recieving the login credentials """
+
+        if self._db.find_user_by(email=email):
+            raise ValueError("User <user's email> already exists")
+        user_password = _hash_password(password)
+        user = User(email=email, user_password=user_password)
+        self._db.add_user(user)
+        return user
