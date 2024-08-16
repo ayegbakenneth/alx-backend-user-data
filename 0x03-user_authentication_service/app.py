8ca3a7485ae96152480ec32bf70e895ad5b60423
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-16 13:31:00 +0000

    An end route that register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 """ File  executable path """
 
+from auth import Auth
 from flask import Flask, jsonify
 """ Module importation path """
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """ a users function that implements
+    the POST /users route."""
+    try:
+        Auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == ("__main__"):
