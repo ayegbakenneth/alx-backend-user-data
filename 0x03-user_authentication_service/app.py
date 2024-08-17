c475db7093a318df137b8a153a7d6c7d21e435b2
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-17 10:56:49 +0000

    Implementation of a login function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,5 +29,20 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", method=["POST"], strict_slashes=False)
+def login() -> str:
+    """ A login function to respond
+    to the POST /sessions route."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    server_response = jsonify(
+            {"email": email, "message": "logged in"})
+    server_response.set_cookie("session_id", session_id)
+    return server_response
+
+
 if __name__ == ("__main__"):
     app.run(host="0.0.0.0", port="5000")
